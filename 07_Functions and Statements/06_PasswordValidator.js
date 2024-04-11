a3696161db70dcc3a8c86add7d2ca905a39cdcf7
Stavromir <s.kasapov@protonmail.com>
2024-04-11 10:27:16 +0300

    task 06

--- 07_Functions and Statements/06_PasswordValidator.js
+++ 07_Functions and Statements/06_PasswordValidator.js
@@ -1,16 +1,18 @@
 function validatePassword(password) {
 
+    let isValid = true;
+
     function isInLengthRange(password) {
         let passLength = password.length;
         return passLength >= 6 && passLength <= 10;
     }
 
     function isOnlyFromLettersAndDigits(password) {
-        let match = password.match(/^[(a-z)(A-Z)(\d)]+$/gm);
+        const regex = /^[(a-z)(A-Z)(\d)]+$/gm;
+        // let match = password.match(regex);
+        let validation = regex.test(password);
 
-        if (match !== null && match.length === 1) {
-            return true;
-        }
+        if (validation) return true;
     }
 
     function haveAtLeastTwoDigits(password) {
@@ -21,14 +23,23 @@ function validatePassword(password) {
     }
 
     if (!isInLengthRange(password)) {
+        isValid = false;
         console.log('Password must be between 6 and 10 characters');
     }
     if (!isOnlyFromLettersAndDigits(password)) {
+        isValid = false;
         console.log("Password must consist only of letters and digits");
     }
     if (!haveAtLeastTwoDigits(password)) {
+        isValid = false;
         console.log("Password must have at least 2 digits");
     }
+
+    if (isValid) {
+        console.log("Password is valid");
+    }
+
+
 }
 
 validatePassword('logIn');
