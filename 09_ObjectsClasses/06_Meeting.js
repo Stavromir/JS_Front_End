4947f0d75aed0e4b0776a2e2117f47b90067f3d1
Stavromir <s.kasapov@protonmail.com>
2024-04-11 14:50:27 +0300

    task 06

---
+++ 09_ObjectsClasses/06_Meeting.js
@@ -0,0 +1,23 @@
+function manageMeetings(array) {
+
+    let calendarManager = {};
+
+    for (const entry of array) {
+        const [key, value] = entry.split(" ");
+
+        if (calendarManager.hasOwnProperty(key)) {
+            console.log(`Conflict on ${key}`);
+        } else {
+            calendarManager[key] = value;
+            console.log(`Scheduled for ${key}`);
+        }
+    }
+
+    Object.keys(calendarManager).forEach(key => console.log(`${key} -> ${calendarManager[key]}`));
+}
+
+manageMeetings(['Monday Peter',
+    'Wednesday Bill',
+    'Monday Tim',
+    'Friday Tim']
+);
