7c519003bef66c35b34dca767fc0331a6de12816
Stavromir Kasapov <127794574+Stavromir@users.noreply.github.com>
2024-04-12 17:45:39 +0300

    add second solution for task 02

--- 11_DomIntro/02.Collect List Items/CollectListItems.js
+++ 11_DomIntro/02.Collect List Items/CollectListItems.js
@@ -6,8 +6,10 @@ function extractText() {
     const items = document.querySelectorAll('#items li');
     const textArea = document.getElementById('result');
 
-    for (const item of Array.from(items)) {
-        textArea.textContent += String(`${item.textContent}\n`);
-    }
+    // for (const item of Array.from(items)) {
+    //     textArea.textContent += String(`${item.textContent}\n`);
+    // }
+
+    textArea.textContent = Array.from(items).map(item => item.textContent).join("\n");
 }
 
