54419056f0c831d882c13d27ca99420370650be8
Stavromir <s.kasapov@protonmail.com>
2024-04-15 15:52:55 +0300

    task 03 and 04 from exercise

--- 11_DomIntro/03. Accordion/accordion.js
+++ 11_DomIntro/03. Accordion/accordion.js
@@ -1,3 +1,14 @@
 function toggle() {
-    console.log('TODO:...');
+    const button = document.querySelector('.button');
+    const text = document.getElementById('extra');
+
+    let buttonLabel = button.textContent;
+
+    if (buttonLabel === "MORE") {
+        text.style.display = 'block';
+        button.textContent = 'LESS';
+    } else {
+        text.style.display = 'none';
+        button.textContent = 'MORE';
+    }
 }
--- 11_DomIntro/04. Search in List/search.js
+++ 11_DomIntro/04. Search in List/search.js
@@ -1,3 +1,25 @@
 function search() {
-   // TODO
+   let listItems = document
+       .getElementById('towns').children;
+
+   let searchInput = document
+       .getElementById('searchText').value;
+
+   let searchResult = document
+       .getElementById('result');
+
+   let searchMatchCount = 0;
+
+    for (let li  of Array.from(listItems)) {
+        li.style.fontWeight = '';
+        li.style.textDecoration = '';
+
+        if (li.textContent.includes(searchInput) && searchInput.length !== 0) {
+            li.style.fontWeight = 'bold';
+            li.style.textDecoration = 'underline';
+            searchMatchCount ++;
+        }
+    }
+
+    searchResult.textContent = `${searchMatchCount} matches found`
 }
