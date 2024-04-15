bab6fffab2446bb71aec04048bfe4998f92b335f
Stavromir <s.kasapov@protonmail.com>
2024-04-15 16:43:18 +0300

    task 05 from exercise

--- 11_DomIntro/05. Table - Search Engine/solution.js
+++ 11_DomIntro/05. Table - Search Engine/solution.js
@@ -1,8 +1,24 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+    document.querySelector('#searchBtn')
+        .addEventListener('click', onClick);
 
-   function onClick() {
-      //   TODO:
+    function onClick() {
 
-   }
+        let itemsList = document.querySelectorAll('tbody tr');
+
+        let searchInput = document.querySelector('#searchField').value;
+
+
+        for (const tr of itemsList) {
+           tr.className = "";
+
+           console.log(tr.innerHTML);
+
+            if (tr.innerHTML.includes(searchInput) && searchInput.length !== 0) {
+                tr.className = "select";
+            }
+        }
+
+        document.querySelector('#searchField').value = "";
+    }
 }
