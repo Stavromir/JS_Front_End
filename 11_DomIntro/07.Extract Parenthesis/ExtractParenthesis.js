2ab96029a62e1d315007a33f5044c80f3d615156
Stavromir <s.kasapov@protonmail.com>
2024-04-15 12:53:01 +0300

    task 07

--- 11_DomIntro/07.Extract Parenthesis/ExtractParenthesis.js
+++ 11_DomIntro/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,22 @@
-function extract(content) {
+function extract(id) {
 
+    const elementText = document
+        .getElementById(id).textContent;
+
+    const pattern = /\((.+)\)/gm;
+
+    // let reg = pattern.exec(elementText);
+    //
+    // console.log(reg.join("; "));
+
+    const match = elementText.match(pattern);
+
+    let countries = [];
+
+    for (const current of match) {
+        let country = current.substring(1, current.length - 1);
+        countries.push(country);
+    }
+
+    return countries.join("; ");
 }
