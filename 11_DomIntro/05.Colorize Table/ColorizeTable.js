ddc8f440b013efe84150cdf9e6600f56d64cf48e
Stavromir <s.kasapov@protonmail.com>
2024-04-15 11:26:30 +0300

    task 05

--- 11_DomIntro/05.Colorize Table/ColorizeTable.js
+++ 11_DomIntro/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,9 @@
 function colorize() {
-    // TODO
+    const tableRows = document.getElementsByTagName('tr');
+
+    for (let i = 1; i <= tableRows.length; i++) {
+        if (i % 2 === 0) {
+            tableRows[i - 1].style.background = 'Teal';
+        }
+    }
 }
