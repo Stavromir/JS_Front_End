d3577ea4364e9a784df2aea7866598d6c13b8331
Stavromir <s.kasapov@protonmail.com>
2024-04-15 12:04:02 +0300

    task 06

--- 11_DomIntro/06.Sum Table/SumTable.js
+++ 11_DomIntro/06.Sum Table/SumTable.js
@@ -1,3 +1,20 @@
 function sumTable() {
 
+    const productValuesRef = document.querySelectorAll('td:nth-child(even)');
+
+    console.log(productValuesRef);
+    const productValues = Array.from(productValuesRef);
+
+    let productsValueSum = 0;
+
+    for (let i = 0; i < productValues.length - 1; i++) {
+
+        console.log(Number(productValues[i]));
+        console.log(Number(productValues[i].textContent));
+
+        productsValueSum += Number(productValues[i].textContent);
+    }
+    console.log(productsValueSum);
+
+    document.getElementById('sum').textContent = productsValueSum;
 }
