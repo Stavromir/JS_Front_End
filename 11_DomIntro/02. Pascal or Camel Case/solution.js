5ac0ab6e1fb9050ec36b5c5b727ee53dbecb7d6c
Stavromir <s.kasapov@protonmail.com>
2024-04-15 14:32:46 +0300

    task 02 from exercises

--- 11_DomIntro/02. Pascal or Camel Case/solution.js
+++ 11_DomIntro/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,42 @@
 function solve() {
-  //TODO...
+
+    const inputText = document
+        .getElementById('text').value;
+    const conventionType = document
+        .getElementById('naming-convention').value;
+
+    let wordsInText = inputText.split(" ");
+
+    let result = "";
+
+    if (conventionType === 'Camel Case') {
+
+        for (let i = 0; i < wordsInText.length; i++) {
+
+            let currentWord = wordsInText[i].toLowerCase();
+
+            if (i > 0) {
+                let firstLetter = currentWord.substring(0, 1);
+                console.log(firstLetter);
+                currentWord = currentWord.replace(firstLetter, firstLetter.toUpperCase());
+            }
+            result += currentWord;
+        }
+
+    } else if (conventionType === 'Pascal Case') {
+
+        for (let i = 0; i < wordsInText.length; i++) {
+
+            let currentWord = wordsInText[i].toLowerCase();
+
+            let firstLetter = currentWord.substring(0, 1);
+            currentWord = currentWord.replace(firstLetter, firstLetter.toUpperCase());
+            result += currentWord;
+        }
+
+    } else {
+        result = 'Error!'
+    }
+
+    document.getElementById('result').textContent = result;
 }
