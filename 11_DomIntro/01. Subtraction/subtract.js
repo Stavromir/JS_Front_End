99c8c66996628bf6e6973a8905ba782e7c5de35c
Stavromir <s.kasapov@protonmail.com>
2024-04-15 13:32:07 +0300

    task 01 from exercises

--- 11_DomIntro/01. Subtraction/subtract.js
+++ 11_DomIntro/01. Subtraction/subtract.js
@@ -1,3 +1,13 @@
 function subtract() {
-    console.log('TODO:...');
+
+    const firstNum = Number(document
+        .getElementById('firstNumber').value);
+    const secondNum = Number(document
+        .getElementById('secondNumber').value);
+
+    const subtractResult = firstNum - secondNum;
+
+    let resultField = document.getElementById('result');
+
+    resultField.textContent = subtractResult;
 }
