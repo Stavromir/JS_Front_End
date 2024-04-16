bb0fe9257a3da901c420d7ccf31228f7078d7b4a
Stavromir <s.kasapov@protonmail.com>
2024-04-16 12:25:32 +0300

    task 03

--- 13_DomEvents/03. Add-Delete/app.js
+++ 13_DomEvents/03. Add-Delete/app.js
@@ -1,3 +1,28 @@
 function addItem() {
-    //TODO...
+
+    let input = document
+        .getElementById('newItemText');
+
+    let listItems = document
+        .getElementById('items');
+
+    let newItem = document.createElement('li');
+    newItem.textContent = input.value;
+
+    const deleteLink = document.createElement('a');
+    deleteLink.textContent = '[Delete]';
+    deleteLink.href = '#'
+
+    deleteLink.addEventListener('click', deleteItem);
+
+    newItem.appendChild(deleteLink);
+
+    listItems.appendChild(newItem);
+
+    input.value = "";
+
+    function deleteItem(e) {
+        let item = e.currentTarget.parentElement;
+        item.remove();
+    }
 }
