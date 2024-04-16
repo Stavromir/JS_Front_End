c3027e19e5d0cd73b67e4130219224713818e40c
Stavromir <s.kasapov@protonmail.com>
2024-04-16 16:56:41 +0300

    task 03

--- 13_DomEvents/03. Locked-Profile/app.js
+++ 13_DomEvents/03. Locked-Profile/app.js
@@ -1,3 +1,30 @@
 function lockedProfile() {
-    console.log('TODO...')
+
+    const profileButtons = document
+        .querySelectorAll('button');
+
+
+    for (let button of profileButtons) {
+        button.addEventListener('click', profileInfo);
+        console.log(button);
+    }
+
+    function profileInfo(e) {
+        const parentDiv = e.currentTarget.parentNode;
+        const buttonText = parentDiv.querySelector('button').textContent;
+        let profileIsLocked = parentDiv
+            .querySelector("input[type='radio']").checked;
+        let hiddenInfo = parentDiv.querySelector('div');
+
+        if (!profileIsLocked && buttonText === 'Show more') {
+            hiddenInfo.style.display = 'block';
+            parentDiv.querySelector('button').textContent = 'Hide it';
+        }
+
+        if (!profileIsLocked && buttonText === 'Hide it') {
+            hiddenInfo.style.display = 'none';
+            parentDiv.querySelector('button').textContent = 'Show more';
+        }
+    }
+
 }
