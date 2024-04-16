2434a972a8846c49245df9c57c6495ff1d18dc3c
Stavromir <s.kasapov@protonmail.com>
2024-04-16 11:59:24 +0300

    task 02

--- 13_DomEvents/02. Delete-From-Table/app.js
+++ 13_DomEvents/02. Delete-From-Table/app.js
@@ -1,3 +1,26 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let input = document
+        .querySelector("input[name='email']");
+
+    let rows = document
+        .querySelectorAll("tbody tr");
+
+    let isRemoved = false;
+
+    for (let row of rows) {
+        if (row.innerHTML.includes(input.value)) {
+            row.remove();
+            isRemoved = true;
+        }
+    }
+
+    let resultField = document
+        .getElementById('result');
+
+    if (isRemoved) {
+        resultField.textContent = 'Deleted'
+        input.value = "";
+    } else {
+        resultField.textContent = 'Not found.'
+    }
 }
