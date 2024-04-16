6823aa644345088e355b06bd53b5031bd23dadd7
Stavromir <s.kasapov@protonmail.com>
2024-04-16 13:05:58 +0300

    task 05

--- 13_DomEvents/05. Highlight-Active/app.js
+++ 13_DomEvents/05. Highlight-Active/app.js
@@ -1,3 +1,19 @@
 function focused() {
-    console.log('TODO:...');
+    const listItems = Array.from(document
+        .getElementsByTagName('input'));
+
+    for (const field of listItems) {
+        field.addEventListener('focus', onFocus);
+        field.addEventListener('blur', onBlur);
+    }
+
+    function onFocus(e) {
+        const parentDivElement = e.currentTarget.parentElement;
+        parentDivElement.className = 'focused';
+    }
+
+    function onBlur(e) {
+        const parentDivElement = e.currentTarget.parentElement;
+        parentDivElement.className = '';
+    }
 }
