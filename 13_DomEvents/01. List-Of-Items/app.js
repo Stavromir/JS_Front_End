4fc89b94c0bbbe8deac7d29372061e38ffb0589b
Stavromir <s.kasapov@protonmail.com>
2024-04-16 11:49:03 +0300

    task 01 from lecture

--- 13_DomEvents/01. List-Of-Items/app.js
+++ 13_DomEvents/01. List-Of-Items/app.js
@@ -1,3 +1,14 @@
 function addItem() {
-    console.log('TODO:...');
+    let input = document
+        .getElementById('newItemText');
+
+    let listItems = document
+        .getElementById('items');
+
+    let newItem = document.createElement('li');
+    newItem.textContent = input.value;
+
+    listItems.appendChild(newItem);
+
+    input.value = "";
 }
