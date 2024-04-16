9a168fbb501d48febe09621d6eb1b67428de74b0
Stavromir <s.kasapov@protonmail.com>
2024-04-16 17:12:00 +0300

    task 04

--- 13_DomEvents/04. Fill-Dropdown/dropdown.js
+++ 13_DomEvents/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,19 @@
 function addItem() {
-    console.log('TODO:...');
+    const inputText = document
+        .getElementById('newItemText');
+    const inputValue = document
+        .getElementById('newItemValue');
+
+    const newElement = document
+        .createElement('option');
+    newElement.textContent = inputText.value;
+    newElement.value = inputValue.value;
+
+    if (inputText.value !== '' && inputValue.value !== '') {
+        document.getElementById('menu')
+            .appendChild(newElement);
+
+        inputText.value = '';
+        inputValue.value = '';
+    }
 }
