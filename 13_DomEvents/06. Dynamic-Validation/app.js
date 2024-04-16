f61a64f7528c33636acd49f5dde265aba9961b50
Stavromir <s.kasapov@protonmail.com>
2024-04-16 13:17:27 +0300

    task 06

--- 13_DomEvents/06. Dynamic-Validation/app.js
+++ 13_DomEvents/06. Dynamic-Validation/app.js
@@ -1,3 +1,17 @@
 function validate() {
-    console.log('TODO:...');
+    const input = document
+        .getElementById('email');
+
+    input.addEventListener('change', onChange);
+
+    function onChange(e) {
+        const element = e.currentTarget;
+        const pattern = /^[a-z]+@[a-z]+\.[a-z]+$/g;
+
+        if (!pattern.test(element.value)) {
+            element.classList.add('error');
+        } else {
+            element.classList.remove('error');
+        }
+    }
 }
