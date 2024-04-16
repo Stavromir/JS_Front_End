5fc619623a9dee0d2411a886b93b0c8beaa850f2
Stavromir <s.kasapov@protonmail.com>
2024-04-16 14:33:02 +0300

    task 07

--- 13_DomEvents/07. Shopping-Cart/solution.js
+++ 13_DomEvents/07. Shopping-Cart/solution.js
@@ -1,3 +1,45 @@
 function solve() {
-   //TODO...
+   const buttonElements = document
+       .querySelectorAll('button.add-product');
+
+   const outputTextElement = document
+       .querySelector('textarea');
+
+   const productsInCart = [];
+   let productsPrice = 0;
+
+   buttonElements.forEach(element => {
+       element.addEventListener('click', addProduct);
+   });
+
+   function addProduct(e) {
+       const product = e.currentTarget.parentNode.parentNode;
+       const productTitle = product
+           .querySelector('.product-title').textContent;
+       const productPrice = Number(product
+           .querySelector('.product-line-price').textContent);
+
+       if (!productsInCart.includes(productTitle)) {
+           productsInCart.push(productTitle);
+       }
+
+       productsPrice += productPrice;
+
+       outputTextElement.textContent += `Added ${productTitle} for ${productPrice.toFixed(2)} to the cart.\n`;
+    }
+
+    const checkoutElement = document.querySelector('.checkout');
+
+    checkoutElement.addEventListener('click', boughtProducts);
+
+    function boughtProducts() {
+
+        buttonElements.forEach(element => {
+            element.removeEventListener('click', addProduct);
+        });
+
+        outputTextElement.textContent += `You bought ${productsInCart.join(', ')} for ${productsPrice}.`
+    }
+
+
 }
