91225c5bc4c67d7eafc3a7a727dc0b626ae50108
Stavromir <s.kasapov@protonmail.com>
2024-04-16 18:07:40 +0300

    task 05

--- 13_DomEvents/05. Encode-and-Decode-Messages/app.js
+++ 13_DomEvents/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,40 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+
+    const textElements = document
+        .querySelectorAll('textarea');
+    const encodeButtonElements = document
+        .querySelectorAll('button');
+
+    const inputTextElement = textElements[0];
+    const outputTextElement = textElements[1];
+
+    const encodeButton = encodeButtonElements[0];
+    const decodeButton = encodeButtonElements[1];
+
+    encodeButton.addEventListener('click', encodeMessage);
+    decodeButton.addEventListener('click', decodeMessage);
+    function encodeMessage() {
+        let textToEncode = inputTextElement.value;
+
+        let encodeMessage = '';
+        for (const letter of textToEncode) {
+            encodeMessage += String.fromCharCode(letter.charCodeAt() + 1);
+        }
+        outputTextElement.value = encodeMessage;
+        inputTextElement.value = '';
+
+        decodeButton.disabled = false;
+    }
+
+    function decodeMessage() {
+        let textToDecode = outputTextElement.value;
+
+        let decodeMessage = '';
+        for (const letter of textToDecode) {
+            decodeMessage += String.fromCharCode(letter.charCodeAt() - 1);
+        }
+        outputTextElement.value = decodeMessage;
+
+        decodeButton.disabled = true;
+    }
 }
