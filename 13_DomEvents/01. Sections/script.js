be908f464aa9c703d13bab05c333a2abe6145fbb
Stavromir <s.kasapov@protonmail.com>
2024-04-16 15:49:35 +0300

    task 01 and 02

--- 13_DomEvents/01. Sections/script.js
+++ 13_DomEvents/01. Sections/script.js
@@ -1,3 +1,22 @@
 function create(words) {
-   console.log('TODO:...');
+   const contentElement = document
+       .getElementById('content');
+
+
+   for (const word of words) {
+      let newDivElement = document.createElement('div');
+      let newParElement = document.createElement('p');
+
+      newParElement.textContent = word;
+      newParElement.style.display = 'none';
+
+      newDivElement.appendChild(newParElement);
+      newDivElement.addEventListener('click', showText);
+      contentElement.appendChild(newDivElement);
+   }
+
+   function showText(e) {
+      const element = e.currentTarget.children[0];
+      element.style.display = '';
+   }
 }
--- 13_DomEvents/02. Time-Converter/solution.js
+++ 13_DomEvents/02. Time-Converter/solution.js
@@ -1,4 +1,46 @@
 function attachEventsListeners() {
 
-    console.log('TODO:...');
+    const daysBtn = document
+        .getElementById('daysBtn');
+    const hoursBtn = document
+        .getElementById('hoursBtn');
+    const minutesBtn = document
+        .getElementById('minutesBtn');
+    const secondsBtn = document
+        .getElementById('secondsBtn');
+
+    daysBtn.addEventListener('click', convertFromDays);
+    hoursBtn.addEventListener('click', convertFromHours);
+    minutesBtn.addEventListener('click', convertFromMinutes);
+    secondsBtn.addEventListener('click', convertFromSeconds);
+
+
+    function convertFromDays(e) {
+        const days = Number(e.currentTarget.parentNode.querySelector('#days').value);
+        document.getElementById('hours').value = days * 24;
+        document.getElementById('minutes').value = days * 24 * 60;
+        document.getElementById('seconds').value = days * 24 * 60 * 60;
+    }
+
+    function convertFromHours(e) {
+        const hours = Number(e.currentTarget.parentNode.querySelector('#hours').value);
+        document.getElementById('days').value =hours / 24;
+        document.getElementById('minutes').value = hours  * 60;
+        document.getElementById('seconds').value = hours * 60 * 60;
+    }
+
+    function convertFromMinutes(e) {
+        const minutes = Number(e.currentTarget.parentNode.querySelector('#minutes').value);
+        document.getElementById('days').value = (minutes / 60) / 24;
+        document.getElementById('hours').value = minutes / 60;
+        document.getElementById('seconds').value = minutes * 24 * 60 * 60;
+    }
+
+    function convertFromSeconds(e) {
+        const seconds = Number(e.currentTarget.parentNode.querySelector('#seconds').value);
+        document.getElementById('days').value = ((seconds / 60) /60) / 24;
+        document.getElementById('hours').value = (seconds / 60) / 60;
+        document.getElementById('minutes').value = seconds / 60;
+
+    }
 }
