2b81c8e9ba409f4e0e0096ef51e8e63b5cdfd00d
Stavromir <s.kasapov@protonmail.com>
2024-04-18 16:08:45 +0300

    Solution for task 02

--- 17_Asynchronous Programming/EXERCISE/02.Messenger/app.js
+++ 17_Asynchronous Programming/EXERCISE/02.Messenger/app.js
@@ -1,5 +1,50 @@
 function attachEvents() {
-    // TODO:
+    const baseUrl = 'http://localhost:3030/jsonstore/messenger';
+    const [authorInput, contentInput, sendButton, refreshButton] = document.getElementsByTagName("input");
+    const textArea = document.getElementById('messages');
+
+    sendButton.addEventListener('click', postNewMassage);
+    refreshButton.addEventListener('click', showAllMassages);
+
+    function postNewMassage(e) {
+        e.preventDefault();
+
+        let requestBody = {
+            author: authorInput.value,
+            content: contentInput.value,
+        }
+
+        const isFilled = authorInput.value !== "" && contentInput.value !== "";
+
+        if (isFilled) {
+            fetch(baseUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(requestBody),
+            })
+                .catch(err => console.log(err));
+        }
+    }
+
+    function showAllMassages(e) {
+        e.preventDefault();
+
+        const allMassages = [];
+
+        fetch(baseUrl)
+            .then(res => res.json())
+            .then(data => {
+                Object.values(data)
+                    .forEach(message => {
+                        allMassages.push(`${message.author}: ${message.content}`);
+                    });
+                textArea.textContent = allMassages.join('\n');
+            })
+            .catch(err => console.log(err));
+    }
+
 }
 
 attachEvents();
